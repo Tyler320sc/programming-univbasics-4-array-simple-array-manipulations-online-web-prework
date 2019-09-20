7f5116ac1da8e833a74c0af948236f43b22eef3e
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-20 01:57:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array, array2)
   array.concat(array2)
 end
 
-def using_insert(array)
-  array.insert(4, "Python")
+def using_insert(array, new_element)
+  array.insert(4, new_element)
 end
 
 # expect(@new_array[4]).to eq(@another_language)
